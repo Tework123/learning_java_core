a0d8bd7366f5161e8a84facbc54bb39f5e5f1a36
Tework123 <daniliesaec@gmail.com>
2023-10-14 09:19:57 +0300

    add 4

--- src/Arrays.java
+++ src/Arrays.java
@@ -21,9 +21,49 @@ public class Arrays {
 //            System.out.println(string);
 //        }
 
-        int x = (int) Math.pow(2, 3);
-        System.out.println(x);
+//        char[][] chars = {{'a', 'b', 'c'}, {'d'}, {'g', 'h'}};
+//        for (int i = 0; i < chars.length; i++) {
+//            System.out.println(chars[i][0]);
+//        }
 
+//        String[][][] strings = new String[1][3][5];
+//        char[][][] chars = {{{'a', 'a', 'a'}}, {{'b'}, {'b', 'b'}}, {{'c', 'c'}}, {{'d'}, {'d'}}};
+//        в первом массиве 2 массива, в каждом из них еще по 3 массива, в каждом из них по 4 элемента
+//        char[][][] chars = new char[2][3][4];
+
+//        for (int i = 0; i < chars.length; i++) {
+//            System.out.println("i start");
+//            System.out.println(java.util.Arrays.deepToString(chars[i]));
+
+//            for (int j = 0; j < chars[i].length; j++) {
+//                System.out.println("j start");
+//                System.out.println(chars[i][j]);
+//
+//                for (int k = 0; k < chars[i][j].length; k++) {
+//                    System.out.println("k start");
+//                    System.out.println(chars[i][j][k]);
+//                }
+//
+//            }
+//        }
+
+
+        int n = 5;
+        int m = 6;
+        // пример заполнения
+        int[][][] mas = new int[2][3][4];
+
+        mas[1] = new int[][]{};
+
+        //пример вывода
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < mas[i].length; j++) //length
+                for (int k = 0; k < mas[i][j].length; k++) {
+                    System.out.println(mas[i][j][k]);
+
+                }
+        }
     }
 }
 
